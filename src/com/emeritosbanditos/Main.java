efd52804ac3a06b280baa86b80abcad306e733cb
Alan <215848@edu.p.lodz.pl>
2020-03-04 13:14:41 +0100

    added github id

--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -3,6 +3,8 @@ package com.emeritosbanditos;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera: Lisek8");
+        System.out.println("GithubID developera: 215848");
     }
 }
