5ae2d6f83aaada7ddf7356ffae374bc6c9b02b95
zientak <215934@edu.p.lodz.pl>
2020-03-04 13:15:56 +0100

    added github id

--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera: Lisek8");
+        System.out.println("GithubID testera: zenakz");
     }
 }
