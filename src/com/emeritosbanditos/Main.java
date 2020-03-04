db920a3e53cac8caf4a4acd5326089fc4affe412
Alan <215848@edu.p.lodz.pl>
2020-03-04 13:40:25 +0100

    diff

--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -6,11 +6,19 @@ public class Main {
         double Addition(double A, double B);
     }
 
+    interface IArithmeticsDiff {
+        double Difference(double A, double B);
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera/operations: Lisek8");
         System.out.println("GithubID developera: Nyitz");
         System.out.println("GithubID developera: 215848");
         System.out.println("GithubID testera: zenakz");
         System.out.println("Github ID developera: 215877");
+
+
+
     }
 }
