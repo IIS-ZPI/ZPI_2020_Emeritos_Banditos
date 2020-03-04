568d027724633bb548d2be6c9f6d59d102d7e2c3
zientak <215934@edu.p.lodz.pl>
2020-03-04 13:42:06 +0100

    Added multiplication

--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -6,6 +6,11 @@ public class Main {
         double Addition(double A, double B);
     }
 
+    interface IArithmeticsMult{
+        double Multiplication(double A, double B);
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera/operations: Lisek8");
         System.out.println("GithubID developera: Nyitz");
