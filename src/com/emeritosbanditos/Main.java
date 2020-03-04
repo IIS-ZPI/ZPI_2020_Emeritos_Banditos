c7b2feea7f9a0d5599aaaabd824c2fb8eb7daccc
Lisek8 <fafik989@gmail.com>
2020-03-04 13:22:03 +0100

    Added role to team leader

--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera: Lisek8");
+        System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera/operations: Lisek8");
         System.out.println("GithubID developera: 215848");
         System.out.println("GithubID testera: zenakz");
 
