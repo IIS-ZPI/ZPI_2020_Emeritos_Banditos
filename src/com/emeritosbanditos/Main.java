938a824f915675911436a03aa522dee1ab2fc296
Lisek8 <zalewski.ukas@gmail.com>
2020-03-04 13:03:05 +0100

    Lab1 Zad1

---
+++ src/com/emeritosbanditos/Main.java
@@ -0,0 +1,8 @@
+package com.emeritosbanditos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera: Lisek8");
+    }
+}
