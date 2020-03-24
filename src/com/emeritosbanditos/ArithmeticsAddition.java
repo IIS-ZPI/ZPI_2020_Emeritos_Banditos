46da59181ce19334dbae83f026e8403d52e420c7
Alan <xgdw11@gmail.com>
2020-03-25 00:25:09 +0100

    Added a comment

--- src/com/emeritosbanditos/ArithmeticsAddition.java
+++ src/com/emeritosbanditos/ArithmeticsAddition.java
@@ -3,6 +3,7 @@ package com.emeritosbanditos;
 public class ArithmeticsAddition implements IArithmeticsAdd {
     @Override
     public double Addition(double a, double b) {
+        // Returns sum of a and b
         return a + b;
     }
 }
