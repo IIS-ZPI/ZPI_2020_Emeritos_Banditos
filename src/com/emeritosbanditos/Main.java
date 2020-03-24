42d683a5a26a0c5b7aee76bf1b9e16fba0300767
215848 <215848@edu.p.lodz.pl>
2020-03-25 00:02:37 +0100

    Diff implementation

--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -1,7 +1,6 @@
 package com.emeritosbanditos;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera/operations: Lisek8");
         System.out.println("GithubID developera: Nyitz");
