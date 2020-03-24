134ebda55687876a8339573bf5c6ae56f260d09e
Zenakz <Zenakz@github.com>
2020-03-25 00:16:54 +0100

    Added a comment

--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -5,7 +5,7 @@ public class Main {//Main class of the project
         System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera/operations: Lisek8");
         System.out.println("GithubID developera: Nyitz");
         System.out.println("GithubID developera: 215848");
-        System.out.println("GithubID testera: zenakz");
+        System.out.println("GithubID testera: zenakz");//Github ID of the tester
         System.out.println("Github ID developera: 215877");
     }
 }
