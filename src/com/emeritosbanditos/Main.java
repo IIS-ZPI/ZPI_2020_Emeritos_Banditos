33319ed7042075d97db2b1a7c8281efd1d051842
215848 <215848@edu.p.lodz.pl>
2020-03-24 23:21:56 +0100

    zad5.3

--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -23,7 +23,7 @@ public class Main {
         System.out.println("GithubID developera: 215848");
         System.out.println("GithubID testera: zenakz");
         System.out.println("Github ID developera: 215877");
-
+        //zad5.3
 
 
     }
