9a8b491bceafaec2e17ede8459d347a426034dbf
215848 <215848@edu.p.lodz.pl>
2020-03-25 00:02:37 +0100

    Diff implementation

---
+++ src/com/emeritosbanditos/ArithmeticsDiff.java
@@ -0,0 +1,8 @@
+package com.emeritosbanditos;
+
+public class ArithmeticsDiff implements IArithmeticsDiff {
+    @Override
+    public double Difference(double A, double B);
+        return A-B;
+    }
+}
--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -1,22 +1,6 @@
 package com.emeritosbanditos;
 
 public class Main {
-
-    interface IArithmeticsAdd {
-        double Addition(double A, double B);
-    }
-
-
-    interface IArithmeticsDiff {
-        double Difference(double A, double B);
-    }
-    
-  interface IArithmeticsMult{
-        double Multiplication(double A, double B);
-
-    }
-
-
     public static void main(String[] args) {
         System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera/operations: Lisek8");
         System.out.println("GithubID developera: Nyitz");
