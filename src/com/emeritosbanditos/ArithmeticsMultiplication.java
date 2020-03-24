c8617f488714edf084f35c3611bdbb9ac1abd3a6
Zenakz <Zenakz@github.com>
2020-03-25 00:49:37 +0100

    Revert "Added a comment"
    This reverts commit 0aea8a9df36536652a62427e3f2f62492693492d.

--- src/com/emeritosbanditos/ArithmeticsMultiplication.java
+++ src/com/emeritosbanditos/ArithmeticsMultiplication.java
@@ -1,6 +1,6 @@
 package com.emeritosbanditos;
 
-public class ArithmeticsMultiplication implements IArithmeticsMult{//komentarz do zadania 6
+public class ArithmeticsMultiplication implements IArithmeticsMult{
     @Override
     public double Multiplication(double a, double b){
         return a * b;
