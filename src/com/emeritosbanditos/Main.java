c765635cc1ca56b2698f68ae21ca3540588c141f
215848 <215848@edu.p.lodz.pl>
2020-03-25 00:24:19 +0100

    zad5.3

--- src/com/emeritosbanditos/Main.java
+++ src/com/emeritosbanditos/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Nazwa grupy: Emeritos Baniditos\nGithubID team leadera/operations: Lisek8");
         System.out.println("GithubID developera: Nyitz");
         System.out.println("GithubID developera: 215848");
-        System.out.println("GithubID testera: zenakz");
+        System.out.println("GithubID testera: zenakz");//zad5.3
         System.out.println("Github ID developera: 215877");
     }
 }
