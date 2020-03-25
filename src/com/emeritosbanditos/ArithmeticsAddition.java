01858c704c24a17b360cc96e3fb1695fec86ea62
Alan <xgdw11@gmail.com>
2020-03-25 01:09:20 +0100

    Revert "Added a comment"
    This reverts commit 518bfdfe

--- src/com/emeritosbanditos/ArithmeticsAddition.java
+++ src/com/emeritosbanditos/ArithmeticsAddition.java
@@ -1,8 +1,6 @@
 package com.emeritosbanditos;
 
 public class ArithmeticsAddition implements IArithmeticsAdd {
-
-    // Adds two numbers together
     @Override
     public double Addition(double a, double b) {
         return a + b;
